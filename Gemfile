73c8264f61dad98599d394d60490923293a7d17e
Devon Blandin <dblandin@gmail.com>
2014-01-18 22:21:46 -0600

    Add sidekiq

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'jquery-rails', '~> 3.0.4'
 gem 'jbuilder',     '~> 1.2'
 gem 'unicorn',      '~> 4.7.0'
 gem 'dotenv-rails', '~> 0.9.0'
+gem 'sinatra',      '~> 1.3.3', require: false
+gem 'sidekiq',      '~> 2.17.3'
 
 group :development do
   gem 'capistrano', '~> 3.0.1'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 MusicData::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  unless Rails.env.test?
+    require 'sidekiq/web'
 
-  # You can have the root of your site routed with "root"
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   root 'welcome#index'
 
   # Example of regular route:
