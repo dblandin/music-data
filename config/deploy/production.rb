d2c2f5e2726ef1f5511568f9a24b0a0b88f90142
Devon Blandin <dblandin@gmail.com>
2014-02-09 10:12:03 -0600

    Add clock role to production server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 set :stage, :production
 set :dotenv, 'config/production.env'
 
-server '192.241.143.82', user: 'deploy', roles: %w[web app db]
+server '192.241.143.82', user: 'deploy', roles: %w[web app db clock]
